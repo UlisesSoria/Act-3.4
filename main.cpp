28ebf2b63b8a290a17959483c51e2bf8a8b54ef1
UlisesSoria <A010704152@tec.mx>
2022-11-18 12:38:32 -0600

    Solution

--- main.cpp
+++ main.cpp
@@ -1,14 +1,41 @@
 // =================================================================
 //
 // File: main.cpp
-// Author:
-// Date:
+// Author: Ulises Soria Baeza
+// Date: 2022/11/18
 //
 // =================================================================
 #include <iostream>
+#include <fstream>
+#include "heap.h"
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
+
+	ifstream infile(argv[1]);
+	ofstream outfile(argv[2]);
+
+	int size, number;
+	infile >> size;
+	Heap<int> heap(size);
+
+	for (int i = 0; i < size; i++)
+	{
+		infile >> number;
+		heap.add(number);
+	}
+
+	int comps = 0;
+	int acum = 0;
+
+	while (heap.getCount() > 1)
+	{
+		comps = heap.remove() + heap.remove();
+		heap.add(comps);
+		acum += comps - 1;
+	}
+	outfile << acum << endl;
 	return 0;
 }
